52568dc4216addbe161a3ef459a5f27cd3e8d925
Michal Zemek <michal@zemek.cz>
2020-03-29 14:13:05 +0200

    Create WhatIUse-ChocolateyScript.bat

---
+++ WhatIUse-ChocolateyScript.bat
@@ -0,0 +1,33 @@
+rem choco install googlechrome
+rem choco install skype
+rem choco install adobereader -y
+rem choco install pdfcreator --ia '"/NOICONS /TASKS=!winexplorer"' -y
+rem choco install visualstudio2019enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US" -y
+rem choco install vscode -y
+
+rem choco install 7zip -y
+rem choco install notepadplusplus.install -y
+rem choco install zoomit -y
+rem choco install qtranslate -y
+rem choco install vlc -y
+rem choco install paint.net -y
+rem choco install imageresizerapp -y
+rem choco install handbrake.install -y
+
+rem choco install audacity -y
+rem choco install audacity-lame -y
+rem choco install audacity-ffmpeg -y
+
+rem choco install streamwriter -y
+
+rem choco install obs-studio -y
+rem choco install audio-router -y
+
+rem choco install nircmd -y
+rem choco install ngrok -y
+
+rem choco install ssms -y
+
+rem choco install fiddler -y
+rem choco install westwindwebsurge -y
+rem choco install sourcetree -y
