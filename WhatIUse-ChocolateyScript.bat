d5f3a53a0e72401085e90c23367dc308e3ce6a29
Michal Zemek <michal@zemek.cz>
2020-03-29 15:14:02 +0200

    Update WhatIUse-ChocolateyScript.bat

--- WhatIUse-ChocolateyScript.bat
+++ WhatIUse-ChocolateyScript.bat
@@ -1,35 +1,28 @@
-rem choco install googlechrome
-rem choco install skype
-rem choco install adobereader -y
-rem choco install pdfcreator --ia '"/NOICONS /TASKS=!winexplorer"' -y
-rem choco install ssms -y
-rem choco install visualstudio2019enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US" -y
-rem choco install vscode -y
-
-rem choco install 7zip -y
-rem choco install notepadplusplus.install -y
-rem choco install zoomit -y
-rem choco install qtranslate -y
-rem choco install vlc -y
-rem choco install paint.net -y
-rem choco install imageresizerapp -y
-rem choco install handbrake.install -y
-
-rem choco install audacity -y
-rem choco install audacity-lame -y
-rem choco install audacity-ffmpeg -y
-
-rem choco install streamwriter -y
-
-rem choco install obs-studio -y
-rem choco install audio-router -y
-
-rem choco install nircmd -y
-rem choco install ngrok -y
-
-
-
-rem choco install fiddler -y
-rem choco install westwindwebsurge -y
-rem choco install sourcetree -y
-rem choco install toggl -y
+choco install googlechrome
+choco install skype
+choco install adobereader -y
+choco install pdfcreator --ia '"/NOICONS /TASKS=!winexplorer"' -y
+choco install ssms -y
+choco install visualstudio2019enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US" -y
+choco install vscode -y
+choco install 7zip -y
+choco install notepadplusplus.install -y
+choco install zoomit -y
+choco install qtranslate -y
+choco install vlc -y
+choco install paint.net -y
+choco install imageresizerapp -y
+choco install handbrake.install -y
+choco install audacity -y
+choco install audacity-lame -y
+choco install audacity-ffmpeg -y
+choco install streamwriter -y
+choco install obs-studio -y
+choco install audio-router -y
+choco install nircmd -y
+choco install ngrok -y
+choco install fiddler -y
+choco install westwindwebsurge -y
+choco install sourcetree -y
+choco install tortoisegit --confirm
+choco install toggl -y
